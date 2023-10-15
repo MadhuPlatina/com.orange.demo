ca4426967417237c5c2fe2b591f410b5c5ab6b05
Venkata Bachu <venkata.bachu@gmail.com>
2023-10-15 18:34:42 -0400

    Dashboard details added

--- src/test/java/testPages/LoginPage.java
+++ src/test/java/testPages/LoginPage.java
@@ -14,6 +14,11 @@ public class LoginPage {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//*[text()='Login']")).click();
+		driver.findElement(By.name("HR")).click();
+		driver.findElement(By.name("Address")).sendKeys("453 pointer ln");
+		driver.findElement(By.name("city")).sendKeys("Atlanta");
+		driver.findElement(By.id("pincode")).sendKeys("675456");
+		driver.findElement(By.name("StreetAddress")).sendKeys("test");
 		
 		
 
