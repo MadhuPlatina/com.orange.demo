14c1846c3b7a287996ed119af5408894ead2c222
Venkata Bachu <venkata.bachu@gmail.com>
2023-10-15 18:23:11 -0400

    Project Structure with gitignore

--- src/test/java/testPages/LoginPage.java
+++ src/test/java/testPages/LoginPage.java
@@ -1,9 +1,16 @@
 package testPages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		
 
 	}
 
